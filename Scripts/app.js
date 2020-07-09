654bfb63fffed0dd332af3799831f97dfe1e7040
psychoCS <thiagoluizb@hotmail.com>
2020-07-09 10:18:23 -0400

    Some changes on the description

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,10 +1,18 @@
+/*
+
+ FILE: COMP125-M2020-MidTerm-Test js
+ AUTHOR: Thiago Luiz Batista
+ WEB SITE: The Travel Report
+ DESCRIPTION: This is the JavaScript file for the whole web page
+
+*/
+
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
-"use strict";
+    "use strict";
 
 
-// YOUR CODE GOES HERE
+    // YOUR CODE GOES HERE
 
 
 })();
-
